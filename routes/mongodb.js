42bab651f5a19147b86e261bcba0705183c87b67
万峰 <iuliao@163.com>
2018-10-30 16:26:03 +0800

    数据库第4次提交

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -4,7 +4,7 @@ let router = express.Router();
 let operatorDB = require('../modules/operatorDB');
 
 router.get('/find',function(req,res,next){
-    operatorDB.find('xrr','xrrCollection','',(err,content)=>{
+    operatorDB.find('wfdb','wfCollection','',(err,content)=>{
         if(!err){
             console.log("444"+JSON.stringify(content));
             res.json(content);
