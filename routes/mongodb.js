4249f04b7989e060a6412454c8a6136268438acb
万峰 <iuliao@163.com>
2018-10-30 16:02:32 +0800

    数据库第三次提交

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -6,7 +6,7 @@ let operatorDB = require('../modules/operatorDB');
 router.get('/find',function(req,res,next){
     operatorDB.find('xrr','xrrCollection','',(err,content)=>{
         if(!err){
-            // console.log("444"+JSON.stringify(content));
+            console.log("444"+JSON.stringify(content));
             res.json(content);
         }
     })
