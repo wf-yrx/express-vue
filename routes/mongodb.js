076ebf764ec9b5ccefa85fe0d82758e7fdb687a4
万峰 <iuliao@163.com>
2018-10-31 17:23:22 +0800

    12321

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -17,7 +17,7 @@ router.get('/find',function(req,res,next){
 
 router.get('/findAccess',function(req,res,next){
     // console.log('获取前端的请求参数',req.query);
-    operatorDB.find('wfdb','wfCollection',{'age':parseInt(req.query.age)},(err,content)=>{
+    operatorDB.find('wfdb','wfCollection',{'age':req.query.age},(err,content)=>{
         // operatorDB.find('xrr','xrrCollection',{'age':parseInt(req.query.age)},(err,content)=>{
         if(!err){
             res.json(content);
@@ -27,7 +27,7 @@ router.get('/findAccess',function(req,res,next){
 
 router.get('/findOne',function(req,res,next){
     // console.log('获取前端的请求参数',req.query);
-    operatorDB.findOne('wfdb','wfCollection',{'age':parseInt(req.query.age)},(err,content)=>{
+    operatorDB.findOne('wfdb','wfCollection',{'age':req.query.age},(err,content)=>{
         // operatorDB.findOne('xrr','xrrCollection',{'age':parseInt(req.query.age)},(err,content)=>{
         if(!err){
             res.json(content);
@@ -47,7 +47,7 @@ router.get('/insertOne',function(req,res,next){
 
 router.get('/updateOne',function(req,res,next){
     // console.log('获取前端的请求参数',req.query);
-   operatorDB.updateOne('wfdb','wfCollection',{'_id':ObjectId(req.query.id)},{$set:{'name':req.query.name,'age':parseInt(req.query.age)}},(err,content)=>{
+   operatorDB.updateOne('wfdb','wfCollection',{'_id':ObjectId(req.query.id)},{$set:{'name':req.query.name,'age':req.query.age}},(err,content)=>{
     // operatorDB.updateOne('xrr','xrrCollection',{'_id':ObjectId(req.query.id)},{$set:{'name':req.query.name,'age':parseInt(req.query.age)}},(err,content)=>{
         if(!err){
             res.json(content);
