2105f25181836667a9d401c5ab5268e8e28d9fe5
万峰 <iuliao@163.com>
2018-10-31 16:57:31 +0800

    用户信息增删改查

--- modules/operatorDB.js
+++ modules/operatorDB.js
@@ -1,13 +1,72 @@
 let connectDb = require('./db');
 
 //查询所有数据
-function find(dbname,collectionName,json,callback){
+// function find(dbname,collectionName,json,callback){
+//     connectDb((db)=>{
+//         let DB = db.db(dbname);
+//         let collection = DB.collection(collectionName);
+//         let result  = collection.find(json).toArray(callback);
+//     })
+// }
+let find = (dbname,collectionName,json,callback)=>{
     connectDb((db)=>{
         let DB = db.db(dbname);
         let collection = DB.collection(collectionName);
-        let result  = collection.find(json).toArray(callback);
+        collection.find(json).toArray(callback);
     })
-}
+};
+
+//添加某一条数据
+let insertOne = (dbname,collectionName,json,callback)=>{
+    connectDb((db)=>{
+        let DB = db.db(dbname);
+        let collection = DB.collection(collectionName);
+        collection.insertOne(json,'',callback);
+    })
+};
+
+//查询某一条数据
+let findOne =(dbname,collectionName,json,callback)=>{
+    connectDb((db)=>{
+        let DB = db.db(dbname);
+        let collection = DB.collection(collectionName);
+        collection.findOne(json,"",callback);
+    })
+};
+
+//修改某一条数据
+let updateOne = (dbname,collectionName,filter,json,callback)=>{
+    connectDb((db)=>{
+        let DB = db.db(dbname);
+        let collection = DB.collection(collectionName);
+        collection.updateOne(filter,json,'',callback);
+    })
+};
+
+//删除某一条数据
+let deleteOne = (dbname,collectionName,filter,callback)=>{
+    connectDb((db)=>{
+        let DB = db.db(dbname);
+        let collection = DB.collection(collectionName);
+        collection.deleteOne(filter,'',callback);
+    })
+};
+
+//删除所有数据
+let remove = (dbname,collectionName,filter,callback)=>{
+    connectDb((db)=>{
+        let DB = db.db(dbname);
+        let collection = DB.collection(collectionName);
+        collection.deleteMany(filter,'',callback);
+    })
+};
+
+
 module.exports = {
     find,
+    findOne,
+    insertOne,
+    updateOne,
+    deleteOne,
+    remove,
 };
--- routes/mongodb.js
+++ routes/mongodb.js
@@ -1,15 +1,72 @@
 let express = require('express');
 let router = express.Router();
 
+let ObjectId = require('mongodb').ObjectID;
+
 let operatorDB = require('../modules/operatorDB');
 
 router.get('/find',function(req,res,next){
-    operatorDB.find('wfdb','wfCollection','',(err,content)=>{
-        if(!err){
+    // operatorDB.find('wfdb','wfCollection','',(err,content)=>{
+       operatorDB.find('xrr','xrrCollection','',(err,content)=>{
+           if(!err){
             console.log("444"+JSON.stringify(content));
             res.json(content);
         }
     })
 });
 
+router.get('/findAccess',function(req,res,next){
+    // console.log('获取前端的请求参数',req.query);
+    operatorDB.find('xrr','xrrCollection',{'age':parseInt(req.query.age)},(err,content)=>{
+        if(!err){
+            res.json(content);
+        }
+    })
+});
+
+router.get('/findOne',function(req,res,next){
+    // console.log('获取前端的请求参数',req.query);
+    operatorDB.findOne('xrr','xrrCollection',{'age':parseInt(req.query.age)},(err,content)=>{
+        if(!err){
+            res.json(content);
+        }
+    })
+});
+
+router.get('/insertOne',function(req,res,next){
+    // console.log('获取前端的请求参数',req.query);
+    operatorDB.insertOne('xrr','xrrCollection',{'name':req.query.name,'age':parseInt(req.query.age)},(err,content)=>{
+        if(!err){
+            res.json(content);
+        }
+    })
+});
+
+router.get('/updateOne',function(req,res,next){
+    // console.log('获取前端的请求参数',req.query);
+    operatorDB.updateOne('xrr','xrrCollection',{'_id':ObjectId(req.query.id)},{$set:{'name':req.query.name,'age':parseInt(req.query.age)}},(err,content)=>{
+        if(!err){
+            res.json(content);
+        }
+    })
+});
+
+router.get('/deleteOne',function(req,res,next){
+    // console.log('获取前端的请求参数',req.query);
+    operatorDB.deleteOne('xrr','xrrCollection',{'_id':ObjectId(req.query.id)},(err,content)=>{
+        if(!err){
+            res.json(content);
+        }
+    })
+});
+
+router.get('/remove',function(req,res,next){
+    // console.log('获取前端的请求参数',req.query);
+    operatorDB.remove('xrr','xrrCollection',{},(err,content)=>{
+        console.log(111,err);
+        if(!err){
+            res.json(content);
+        }
+    })
+});
 module.exports = router;
