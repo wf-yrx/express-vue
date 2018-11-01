b936c100ef3618accea9db55fb918b626886bb42
万峰 <iuliao@163.com>
2018-11-01 14:23:28 +0800

    点滴故事修改2

--- modules/db.js
+++ modules/db.js
@@ -1,8 +1,8 @@
 var MongoCient = require('mongodb').MongoClient;
 // var dbUrl = 'mongodb://wf_runner:zff1314520hh@wf.e2e38.com:27138/wfdb'; //远程mongodb数据库
 //  'mongo wf.e2e38.com:27138/wfdb -u wf_runner -p zff1314520hh';  指令远程连接服务器数据库
-// var dbUrl = 'mongodb://wf_runner:zff1314520hh@localhost:27138/wfdb';//远程mongodb数据库
-var dbUrl = 'mongodb://localhost:27017/xrr';//本地数据库
+var dbUrl = 'mongodb://wf_runner:zff1314520hh@localhost:27138/wfdb';//远程mongodb数据库
+// var dbUrl = 'mongodb://localhost:27017/xrr';//本地数据库
 
 
 function connectDb(callback){
