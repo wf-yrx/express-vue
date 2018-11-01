6d3534d612106c0e24503cc85ce2c27abcd76bcd
万峰 <iuliao@163.com>
2018-11-01 18:14:09 +0800

    555

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'views')));
 
-app.use('/*', indexRouter);
+
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api',api);
 app.use('/mongodb',mongodb);
