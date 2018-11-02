c8cb75d94bae5b9ec855c898b571e298aa5ceaf6
万峰 <iuliao@163.com>
2018-11-02 16:40:14 +0800

    服务端重新部署

--- modules/db.js
+++ modules/db.js
@@ -1,6 +1,6 @@
 var MongoCient = require('mongodb').MongoClient;
 // var dbUrl = 'mongodb://wf_runner:zff1314520hh@wf.e2e38.com:27138/wfdb'; //远程mongodb数据库
-//  'mongo wf.e2e38.com:27138/wfdb -u wf_runner -p zff1314520hh';  指令远程连接服务器数据库
+//  'mongo 127.0.0.1/wfdb -u wf_runner -p zff1314520hh';  指令远程连接服务器数据库
 var dbUrl = 'mongodb://wf_runner:zff1314520hh@localhost:27138/wfdb';//远程mongodb数据库
 // var dbUrl = 'mongodb://localhost:27017/xrr';//本地数据库
 
